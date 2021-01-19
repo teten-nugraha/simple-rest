0abc54767a84f0e440be643c4991f510ee8655ed
Teten Nugraha <teten.nugraha18@gmail.com>
2021-01-19 06:52:51 +0000

    Initial commit

---
+++ src/main/java/id/ten/Application.java
@@ -0,0 +1,10 @@
+package id.ten;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
---
+++ src/test/java/id/ten/SimpleRestTest.java
@@ -0,0 +1,21 @@
+package id.ten;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+public class SimpleRestTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
